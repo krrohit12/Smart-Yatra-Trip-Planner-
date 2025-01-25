afe7e57a412c96de1195cf38d0797f2060939951
krrohit12 <krrohit.2345@gmail.com>
2025-01-25 16:44:42 +0530

    dialog updated

--- src/components/custom/Header.jsx
+++ src/components/custom/Header.jsx
@@ -25,18 +25,18 @@ const Header = () => {
   useEffect(() => {
     console.log(user)
   }, [])
-  const login=useGoogleLogin({
-    onSuccess:(resp)=>getuserprofile(resp),
-    onError:(e)=>console.log(e)
+  const login = useGoogleLogin({
+    onSuccess: (resp) => getuserprofile(resp),
+    onError: (e) => console.log(e)
   })
-  const getuserprofile=(tokenInfo)=>{
-    axios.get(`https://www.googleapis.com/oauth2/v1/userinfo?access_token=${tokenInfo?.access_token}`,{
-      headers:{
-        Authorization:`Bearer ${tokenInfo?.access_token}`,
-        Accept:'Application/json'
+  const getuserprofile = (tokenInfo) => {
+    axios.get(`https://www.googleapis.com/oauth2/v1/userinfo?access_token=${tokenInfo?.access_token}`, {
+      headers: {
+        Authorization: `Bearer ${tokenInfo?.access_token}`,
+        Accept: 'Application/json'
       }
-    }).then((resp)=>{
-      localStorage.setItem('user',JSON.stringify(resp.data))
+    }).then((resp) => {
+      localStorage.setItem('user', JSON.stringify(resp.data))
       setdialog(false);
       window.location.reload();
       // console.log(resp)
@@ -50,17 +50,17 @@ const Header = () => {
         {user ?
           <div className='flex gap-1 items-center'>
             <a href="/create-trip">
-            <Button variant="outline" className="rounded-full">+ Create trip</Button>
+              <Button variant="outline" className="rounded-full">+ Create trip</Button>
             </a>
             <a href="/mytrips">
-            <Button variant="outline" className="rounded-full">Trip History</Button>
+              <Button variant="outline" className="rounded-full">Trip History</Button>
             </a>
             <Popover>
               <PopoverTrigger>
-              <img src={user?.picture} alt="" className='w-[35px] h-[35px] rounded-full my-1' />
+                <img src={user?.picture} alt="" className='w-[35px] h-[35px] rounded-full my-1' />
               </PopoverTrigger>
               <PopoverContent>
-                <h2 onClick={()=>{
+                <h2 onClick={() => {
                   googleLogout();
                   localStorage.clear();
                   window.location.reload();
@@ -68,26 +68,30 @@ const Header = () => {
               </PopoverContent>
             </Popover>
 
-            
+
           </div> :
-          <Button onClick={()=>{
+          <Button onClick={() => {
             setdialog(true);
           }}>Sign in</Button>
         }
 
       </div>
-      <Dialog open={dialog}>
+      <Dialog open={dialog} onOpenChange={(open) => setdialog(open)}>
         <DialogContent>
           <DialogHeader>
-            <DialogDescription> 
-              <img src="/logo.svg" alt="" />
-              <h2 className='font-bold text-lg mt-6'>Sign In with Google</h2>
-              <p >Sign in to the App with Google Autentication</p>
-              <Button onClick={login} className="w-full mt-5" >Sign in with Google</Button>
-            </DialogDescription>
+            <DialogTitle>Sign In</DialogTitle>
           </DialogHeader>
+          <DialogDescription>
+            <img src="/logo.svg" alt="" />
+            <h2 className="font-bold text-lg mt-6">Sign In with Google</h2>
+            <p>Sign in to the App with Google Authentication</p>
+            <Button onClick={login} className="w-full mt-5">
+              Sign in with Google
+            </Button>
+          </DialogDescription>
         </DialogContent>
       </Dialog>
+
     </div>
   )
 }
